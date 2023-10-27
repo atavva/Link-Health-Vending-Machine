abbe9cc940cca226628808d34db6905409408817
theArctesian <stephen.d.okita@gmail.com>
2023-10-26 19:53:04 -0700

    did not do much but there is the beginings of a nav bar

--- Frontend/src/routes/+layout.svelte
+++ Frontend/src/routes/+layout.svelte
@@ -1,5 +1,25 @@
 <script>
   import "../app.css";
+  import languageIcon from "../Images/language.svg";
 </script>
 
+<div class="nav">
+  <div>
+    <h1>Logo</h1>
+  </div>
+  <div>
+    <img src="../Images/language.svg" alt="Change Language" />
+    <h1>文<sub>A</sub></h1>
+    <h1>All Programs</h1>
+    <h1>Users</h1>
+  </div>
+</div>
 <slot />
+
+<style>
+  .nav {
+    background-color: var(--col-pri);
+    color: var(--col-txt);
+    display: flex;
+  }
+</style>
--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,3 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import "../app.css"
+</script>
+<h1 class="m-4">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+<style>
+    h1{
+        color: var(--col-acc);
+    }
+</style>
