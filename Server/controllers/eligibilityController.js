708ee9e8834dc7b385f7e0bd9d2f13bfc3c71d3e
Noah <na933950@gmail.com>
2023-11-02 16:49:31 -0700

    Added eligibility stuff

--- Server/controllers/eligibilityController.js
+++ Server/controllers/eligibilityController.js
@@ -2,6 +2,7 @@ const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 const determineEligibilityForAllPrograms = require("../utils/determineEligibilty");
 const supabase = require("../utils/client");
+const determineBestNextQuestion = require('../utils/determineNextQuestion');
 
 /**
  * Determines the best next question to ask a user to efficiently determine their eligibility
@@ -19,5 +20,19 @@ exports.determineNextQuestion = catchAsync(async (req, res, next) => {
         }
     }
     */
+
+    // List of programs
+    const unkownPrograms = determineUnknownPrograms(userInfo.eligiblePrograms, userInfo.ineligiblePrograms);
+
+    const bestNextEligibility = determineBestNextQuestion(unknownPrograms, userInfo.eligibility);
+
+    const questionInfo = getQuestionInfo(bestNextEligibility);
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            ...questionInfo
+        }
+    })
 
 });
---
+++ Server/utils/determineNextQuestion.js
@@ -0,0 +1,10 @@
+module.exports = (unknownPrograms, eligibility) => {
+    /*
+    eligibility = {
+        "agi": 3000,
+        "dependencies": 5
+    }
+    */
+
+    // Returns the best thing to ask (an example is it could return "agi")
+}
