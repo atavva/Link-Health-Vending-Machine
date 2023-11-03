ec879b4ec1e583c6132f95bcddd056eec7255f84
Noah <na933950@gmail.com>
2023-11-02 19:02:44 -0700

    Set up code for Mia with fixProgramNesting.js

--- Server/utils/determineNextQuestion.js
+++ Server/utils/determineNextQuestion.js
@@ -1,10 +1,16 @@
 module.exports = (unknownPrograms, eligibility) => {
-    /*
+  /*
     eligibility = {
         "agi": 3000,
         "dependencies": 5
     }
     */
+  // Returns the best thing to ask (an example is it could return "agi")
+};
 
-    // Returns the best thing to ask (an example is it could return "agi")
-}
+/**
+ *
+ * @param {Programs[]} cleanedPrograms a list of programs that have been cleaned by fixProgramNesting
+ * @returns {string} the name of the most valued eligibility to know given the heuristic in the function
+ */
+const evaluateBestQuestion = (cleanedPrograms) => {};
