f1289ded61c9580dd77c1ddb2a4c3ea20cd57e74
Noah <na933950@gmail.com>
2023-11-12 19:01:20 -0800

    Implemented Minor Changes

--- Server/controllers/eligibilityController.js
+++ Server/controllers/eligibilityController.js
@@ -3,8 +3,26 @@ const catchAsync = require("../utils/catchAsync");
 const determineEligibilityForAllPrograms = require("../utils/determineEligibility");
 const supabase = require("../utils/client");
 const determineBestNextQuestion = require("../utils/determineNextQuestion");
+const { findKeyWithHighestValue } = require("../utils/determineNextQuestion");
 const { allPrograms } = require("./programController");
 
+exports.getFieldNames = catchAsync(async (req, res, next) => {
+  const { data, error } = await supabase.from('eligibility').select('"Field Name"');
+
+  if (error) {
+    return next(new AppError(error.message, error.code));
+  }
+
+  const fieldNames = []
+  data.forEach((el) => {
+    fieldNames.push(el["Field Name"]);
+  });
+
+  res.status(200).json({
+    status: 'success',
+    fieldNames
+  })
+})
 /**
  * Determines the best next question to ask a user to efficiently determine their eligibility
  */
@@ -27,12 +45,23 @@ exports.determineNextQuestion = catchAsync(async (req, res, next) => {
     userInfo.ineligiblePrograms
   );
 
-  const bestNextEligibility = determineBestNextQuestion(
+  const bestNextEligibilityScores = determineBestNextQuestion(
     unknownPrograms,
     userInfo.eligibility
   );
 
-  const questionInfo = await getQuestionInfo(bestNextEligibility);
+  let questionInfo;
+
+  while (Object.keys(bestNextEligibilityScores).length != 0) {
+    let bestNextFieldName = findKeyWithHighestValue(bestNextEligibilityScores);
+    questionInfo = await getQuestionInfo(bestNextFieldName);
+
+    if (Object.keys(questionInfo).length != 0) {
+      break;
+    }
+
+    delete bestNextEligibilityScores[bestNextFieldName];
+  }
 
   if ("status" in questionInfo) {
     return next(
@@ -40,10 +69,25 @@ exports.determineNextQuestion = catchAsync(async (req, res, next) => {
     );
   }
 
+  const { data: totalCountData, error: totalCountError } = await supabase.from("eligibility").select("count");
+
+  const totalQuestions = totalCountData[0].count || NaN; 
+
+  const { data: remainingQuestionData, error: remainingQuestionError } = await supabase
+    .from("eligibility")
+    .select('count')
+    .in("Field Name", Object.keys(bestNextEligibilityScores));
+
+  const maxRemainingQuestions = remainingQuestionData[0].count || NaN;
+
+  const percentageCompleted = (totalQuestions - maxRemainingQuestions) / totalQuestions;
+
   res.status(200).json({
     status: "success",
     data: {
-      ...questionInfo[0],
+      questionInfo: { ...questionInfo[0] },
+      maxRemainingQuestions,
+      percentageCompleted,
     },
   });
 });
@@ -67,9 +111,9 @@ const determineUnknownPrograms = async (
 
 const getQuestionInfo = async (fieldName) => {
   const { data, error } = await supabase
-  .from("eligibility")
-  .select("*")
-  .eq("Field Name", fieldName);
+    .from("eligibility")
+    .select("*")
+    .eq("Field Name", fieldName);
 
   if (error) {
     return {
