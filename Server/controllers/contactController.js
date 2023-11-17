820363a2d98c2c340a695b172cc0ff86a24d80aa
yoshi-p27 <aayushpatel27@gmail.com>
2023-11-17 11:43:36 -0800

    first version with OAuth2

--- Server/controllers/contactController.js
+++ Server/controllers/contactController.js
@@ -4,53 +4,35 @@ const nodemailer = require('nodemailer');
 exports.sendContactFormInfo = catchAsync(async (req, res, next) => {
 
 
-    req.query = {
-        firstName: string,
-        lastName: string,
-        email: string,
-        message: string,
-    }
-    
-    const contactInfo = req.query;
+    const contactInfo = req.body;
     // Send an email to Link Health via contact form with info in the req object using nodemailer
 
-        
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            type: 'OAuth2',
+            clientId: '494778911159-1lge28b42ga3ra29po8m58er87ninojs.apps.googleusercontent.com',
+            clientSecret: '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',
+            user: 'linkhealthdummy@gmail.com',
+            pass: 'linkhealthdummy123'
+        }
 
+    });
 
+    const info = await transporter.sendMail({
+        from: 'test <linkhealthdummy@gmail.com>',
+        to: 'test <linkhealthdummy@gmail.com>',
 
-
-   
-    async function main(){
-
-        const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                user: 'linkhealthdummy@gmail.com',
-                pass: 'linkhealthdummy123'
-            }
-
-        });
-
-        const info = await transporter.sendMail({
-            from: 'test <linkhealthdummy@gmail.com>',
-            to: 'test <linkhealthdummy@gmail.com>',
-
-            subject: `${contactInfo.firstName} ${contactInfo.lastName} linkhealth message`, //should be some string constructed from req
-            text: `${contactInfo.message}`, //some version of req/res
-            // html: html
-        })
-
-        console.log("message sent" + info.messageID);
-        console.log(info.accepted);
-        console.lob(info.rejected);
-
-    }
-
-    main()
-    .catch(e=>console.log(e));
-
+        subject: `${contactInfo.firstName} ${contactInfo.lastName} linkhealth message`, //should be some string constructed from req
+        text: `${contactInfo.message}`, //some version of req/res
+        // html: html
+    })
+    
+    res.status(201).json({
+        status: 'success',
+    });
 
 })
 
